bea7b8cb1b089d6f751c9bf704a14e366163392f
Marinayanga <marinayanga@gmail.com>
2021-11-07 16:40:57 +0300

    Урок 1. Тест 2

--- src/MainClassTest.java
+++ src/MainClassTest.java
@@ -8,4 +8,11 @@ public class MainClassTest extends MainClass{
     {
         Assert.assertTrue( "local number не равен 14",this.getLocalNumber() ==14);
     }
+
+    @Test
+    public void testGetClassNumber()
+    {
+        Assert.assertTrue( "class_number должен быть больше 45",this.getClassNumber() > 45);
+
+    }
 }
